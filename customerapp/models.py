73509e8f0f26993856a02744deed1a79d90c79cd
Eko Wibowo <swdev.bali@gmail.com>
2019-10-30 10:44:22 +0700

    migrations for customer model

--- customerapp/models.py
+++ customerapp/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 
+JENIS_KELAMIN = (
+    ('L', 'Laki-Laki'),
+    ('P', 'Perempuan'),
+)
+
+OCCUPATION = (
+    ('n', 'Non-Fix'),
+    ('f', 'Fix')
+)
 # Create your models here.
 class Customer(models.Model):
     nama = models.fields.CharField(max_length=255)
+    jenis_kelamin = models.fields.CharField(max_length=1, choices=JENIS_KELAMIN, default='L')
+    gaji = models.fields.FloatField()
+    existing = models.fields.BooleanField()
+    occupation = models.fields.CharField(max_length=1, choices=OCCUPATION, default='n') # FIX, NONFIX
+    alamat = models.fields.TextField()
 
     def __str__(self):
         return self.nama
